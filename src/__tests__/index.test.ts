3137a0d376421ef4110fa34870ce8ac45b6f6ce9
onesyx <alexis.decker.aa@gmail.com>
2023-08-13 02:23:36 +0800

    Fixed initial test files

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -8,28 +8,28 @@ describe("format currency values with Rp symbols", function () {
     });
 
     test("should format with informal notation", function () {
-        expect(RpFmt(10000.55, { formalNotation: false })).toBe("Rp 10.000,55");
-        expect(RpFmt(10000.5, { formalNotation: false })).toBe("Rp 10.000,50");
-        expect(RpFmt(10000, { formalNotation: false })).toBe("Rp 10.000,00");
+        expect(RpFmt(10000.55, { formal: false })).toBe("Rp 10.000,55");
+        expect(RpFmt(10000.5, { formal: false })).toBe("Rp 10.000,50");
+        expect(RpFmt(10000, { formal: false })).toBe("Rp 10.000,00");
     });
 });
 
 describe("format currency values with IDR symbols", function () {
     test("should format with formal notation", function () {
-        expect(RpFmt(10000.55, { currencyType: "IDR" })).toBe("10.000,55 IDR");
-        expect(RpFmt(10000.5, { currencyType: "IDR" })).toBe("10.000,50 IDR");
-        expect(RpFmt(10000, { currencyType: "IDR" })).toBe("10.000,00 IDR");
+        expect(RpFmt(10000.55, { currency: "IDR" })).toBe("10.000,55 IDR");
+        expect(RpFmt(10000.5, { currency: "IDR" })).toBe("10.000,50 IDR");
+        expect(RpFmt(10000, { currency: "IDR" })).toBe("10.000,00 IDR");
     });
 
     test("should format with informal notation", function () {
-        expect(
-            RpFmt(10000.55, { currencyType: "IDR", formalNotation: false }),
-        ).toBe("IDR 10.000,55");
-        expect(
-            RpFmt(10000.5, { currencyType: "IDR", formalNotation: false }),
-        ).toBe("IDR 10.000,50");
-        expect(
-            RpFmt(10000, { currencyType: "IDR", formalNotation: false }),
-        ).toBe("IDR 10.000,00");
+        expect(RpFmt(10000.55, { currency: "IDR", formal: false })).toBe(
+            "IDR 10.000,55",
+        );
+        expect(RpFmt(10000.5, { currency: "IDR", formal: false })).toBe(
+            "IDR 10.000,50",
+        );
+        expect(RpFmt(10000, { currency: "IDR", formal: false })).toBe(
+            "IDR 10.000,00",
+        );
     });
 });
